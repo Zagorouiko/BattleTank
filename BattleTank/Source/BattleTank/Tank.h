639f39dca65da0e709b8e9b3a779f1d7c4431630
Zagorouiko <Zagorouiko@gmail.com>
2019-03-12 07:58:02 -0700

    Added tank aiming component to tank

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -16,7 +17,7 @@ public:
 
 
 protected:
-
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	void AimAt(FVector HitLocation);

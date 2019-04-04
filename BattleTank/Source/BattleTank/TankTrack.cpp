0aaee0eec5aae250694637a7055d4ad3bfbcb26d
Zagorouiko <Zagorouiko@gmail.com>
2019-04-04 08:39:13 -0700

    Fixed the throttle input controls. Added the ability to add a force on the tank body via input.

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -5,4 +5,12 @@
 void UTankTrack::SetThrottle(float Throttle) {
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+
+	//Gets root component(tank BP). then goes down to the "tank"(static mesh), then casts the static mesh(tank) to a primitive (up the hierarchy)
+	//Must be of type primitive to add a force to the static mesh
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
